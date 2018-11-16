f3116aa8cea8d5e49a06c9e8cc68da3e10466033
Vikram Ojha <sc.vikram.ojhja@gmail.com>
2018-11-16 11:35:40 +0530

    Hello World Program for Javascript

---
+++ HelloWorld.js
@@ -0,0 +1,20 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+(function(){
+  "use strict";
+  
+  /*Start of code*/
+  function greet(yourname){
+    alert("Hello " + yourname);
+  }
+  
+  greet("World");
+  /*End of code*/
+}) ();
