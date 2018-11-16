03c6059455a4f121fe1539c044b0517889b26b85
Vikram Ojha <sc.vikram.ojhja@gmail.com>
2018-11-16 12:01:47 +0530

    JavaScript file displaying very basic concept of Array.

---
+++ Array.js
@@ -0,0 +1,27 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+(function(){
+  "use strict";
+  
+  /*Start of code*/
+  function Array(){
+  const MYARR = ["Hello", "World"];
+  MYARR.pop();
+  MYARR.push("Vikram");
+  console.log(MYARR);
+  }
+  
+  Array();
+  /*End of code*/
+}) ();
+/*
+***Output****
+Array [ "Hello", "Vikram" ]
+*/
