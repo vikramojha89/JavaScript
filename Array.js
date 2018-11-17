2df97979d9d9ef5f1cdfe4cd79fc587eeb74927f
Vikram Ojha <sc.vikram.ojha@gmail.com>
2018-11-17 15:14:44 +0530

    Added array functions

--- Array.js
+++ Array.js
@@ -9,6 +9,62 @@
 
 (function(){
   "use strict";
+ 
+  
+  //newline(); /* Function declaration (function hoisting)*/
+  
+  var fruit = ["Apple", "Banana"]; /*Array Literal*/
+  function ModifyArray(){
+    
+    /*Initial array*/
+    console.log("Initial Array");
+    console.log(fruit);
+    /*Output: ['Apple', 'Banana']*/
+    
+    /*For new line*/
+    newline();
+    
+    /*Removing **front** Element from an array*/
+    fruit.shift();
+    console.log("Removing first element from the array");
+    console.log(fruit);
+    /*Output: ['Banana']*/
+    
+    /*for new line*/
+    newline();
+    
+    /*Adding Element to the front of Array*/
+    console.log("Adding Element to the front of Array");
+    fruit.unshift('Orange');
+    console.log(fruit);
+    /*Output: ['Orange', 'Banana']*/
+    
+    /*for new line*/
+    newline();
+    
+    /*Removing last element of array*/
+    console.log("Removing last element of array");
+    fruit.pop();
+    console.log(fruit);
+    /*Output: ['Orange']*/
+    
+    /*for new line*/
+    newline();
+    
+    /*Adding element to the end of Array*/
+    console.log("Adding element to the end of Array");
+    fruit.push('Pineapple');
+    console.log(fruit);
+    /*Output: ['Orange', 'Pineapple']*/
+    
+  }
+  
+  /*For newline*/
+  function newline(){
+    console.log("");
+  }
+  
+  ModifyArray();
   
   /*Start of code*/
   function Array(){
@@ -20,6 +76,8 @@
   
   Array();
   /*End of code*/
+  
+  
 }) ();
 /*
 ***Output****
